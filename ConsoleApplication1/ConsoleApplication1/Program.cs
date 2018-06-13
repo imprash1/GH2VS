b93c2a5efad03db2ee18614d94b2ed5c1fc2c18c
Prashanth Palaniswamy <pp4m7@mst.edu>
2018-06-13 18:03:22 -0500

    b3

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Master");
             Console.WriteLine("inside branch");
+            Console.WriteLine("b3");
         }
     }
 }
